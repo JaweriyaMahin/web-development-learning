92efb7f45a1c092c7b0478d5448d10d3ec2a6f57
JaweriyaMahin <jaweriyamahin786@gmail.com>
2026-03-13 16:51:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,41 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
-let attempts = 0;
-
-function checkGuess(){
-
-let userGuess = document.getElementById("guessInput").value;
-let message = document.getElementById("message");
-
-attempts++;
-document.getElementById("attempts").textContent = attempts;
-
-if(userGuess == randomNumber){
-message.textContent = "🎉 Correct! You guessed it!";
-}
-else if(userGuess > randomNumber){
-message.textContent = "Too High! Try again.";
-}
-else{
-message.textContent = "Too Low! Try again.";
+body{
+font-family: Arial;
+background: linear-gradient(120deg,#74ebd5,#9face6);
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+margin:0;
 }
 
+.weather-container{
+background:white;
+padding:30px;
+border-radius:10px;
+text-align:center;
+box-shadow:0 5px 10px rgba(0,0,0,0.2);
 }
 
-function restartGame(){
-randomNumber = Math.floor(Math.random() * 100) + 1;
-attempts = 0;
-
-document.getElementById("attempts").textContent = 0;
-document.getElementById("message").textContent = "";
-document.getElementById("guessInput").value = "";
+input{
+padding:10px;
+font-size:16px;
+width:200px;
+margin-top:10px;
 }
+
+button{
+padding:10px 20px;
+background:#3498db;
+color:white;
+border:none;
+border-radius:5px;
+cursor:pointer;
+margin-left:5px;
+}body{
+font-family: Arial;
+background: linear-gradient(120deg,#74ebd5,#9face6);
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+margin:
