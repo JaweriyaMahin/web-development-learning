0b7f6e8f4eb42085f628618877a9994751b8392f
JaweriyaMahin <jaweriyamahin786@gmail.com>
2026-03-13 16:53:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,17 @@
-body{
-font-family: Arial;
-background: linear-gradient(120deg,#74ebd5,#9face6);
-height:100vh;
-display:flex;
-justify-content:center;
-align-items:center;
-margin:0;
-}
 
-.weather-container{
-background:white;
-padding:30px;
-border-radius:10px;
-text-align:center;
-box-shadow:0 5px 10px rgba(0,0,0,0.2);
-}
+async function getWeather(){
 
-input{
-padding:10px;
-font-size:16px;
-width:200px;
-margin-top:10px;
-}
+let city = document.getElementById("cityInput").value;
 
-button{
-padding:10px 20px;
-background:#3498db;
-color:white;
-border:none;
-border-radius:5px;
-cursor:pointer;
-margin-left:5px;
-}body{
-font-family: Arial;
-background: linear-gradient(120deg,#74ebd5,#9face6);
-height:100vh;
-display:flex;
-justify-content:center;
-align-items:center;
-margin:
+let apiKey = "YOUR_API_KEY";
+
+let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+
+let response = await fetch(url);
+let data = await response.json();
+
+document.getElementById("cityName").innerText = data.name;
+document.getElementById("temperature").innerText = "Temperature: " + data.main.temp + "°C";
+document.getElementById("description").innerText = "Weather: " + data.weather[0].description;
+
+}
